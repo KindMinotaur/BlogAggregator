34be9fdb19ed364ffe003fb75ff0815f86e7a611
Shaun Belknap <shaun.belknap@gmail.com>
2026-01-03 09:20:16 -0600

    Adjusted to not have main.go be so bloated and added two more helper files

--- main.go
+++ main.go
@@ -1,76 +1,39 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
-
 	"home/gingersnap/Projects/BlogAggregator/internal/config"
 )
 
 type state struct {
-	config *config.Config
-}
-
-type command struct {
-	name      string
-	arguments []string
-}
-
-func handlerLogin(s *state, cmd command) error {
-	if cmd.arguments == nil {
-		log.Fatalln("Please enter a username")
-	}
-	err := s.config.SetUser(cmd.arguments[0])
-	if err != nil {
-		log.Fatalf("Couldn't login user: %v", err)
-	}
-	fmt.Printf("Logged in user: %v", cmd.arguments)
-	return nil
-}
-
-type commands struct {
-	c map[string]func(*state, command) error
-}
-
-func (c *commands) run(s *state, cmd command) error {
-	if f, ok := c.c[cmd.name]; ok {
-		return f(s, cmd)
-	}
-	log.Fatalf("Unknown command: %v", cmd.name)
-}
-
-func (c *commands) register(name string, f func(*state, command) error) {
-	if c.c == nil {
-		c.c = make(map[string]func(*state, command) error)
-	} else if _, exists := c.c[name]; exists {
-		log.Fatalf("Command %v already registered", name)
-	}
-	c.c[name] = f
+	cfg *config.Config
 }
 
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
-		log.Fatalf("Couldn't read config: %v", err)
+		log.Fatalf("error reading config: %v", err)
 	}
 
-	s := &state{
-		config: &cfg,
+	programState := &state{
+		cfg: &cfg,
 	}
-	
-	cmds := &commands{}
+
+	cmds := commands{
+		registeredCommands: make(map[string]func(*state, command) error),
+	}
 	cmds.register("login", handlerLogin)
-	args := os.Args[1:]
-	if len(args) < 2 {
-		log.Fatalln("Please provide a command")
+
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: cli <command> [args...]")
 	}
-	cmd := command{
-		name:      args[0],
-		arguments: args[1:],
-	}
-	err = cmds.run(s, cmd)
+
+	cmdName := os.Args[1]
+	cmdArgs := os.Args[2:]
+
+	err = cmds.run(programState, command{Name: cmdName, Args: cmdArgs})
 	if err != nil {
-		log.Fatalf("Command %v failed: %v", cmd.name, err)
+		log.Fatal(err)
 	}
 }
