864e5505ece9963f7abcc63a2d500d9da934ebfb
Shaun Belknap <shaun.belknap@gmail.com>
2026-01-02 19:12:06 -0600

    Built commands structs and handlerLogin method

--- main.go
+++ main.go
@@ -3,8 +3,42 @@ package main
 import (
 	"fmt"
 	"log"
+
 	"home/gingersnap/Projects/BlogAggregator/internal/config"
 )
+
+type state struct {
+	config *config.Config
+}
+
+type command struct {
+	name      string
+	arguments []string
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if cmd.arguments == nil {
+		log.Fatalln("Please enter a username")
+	}
+	err := s.config.SetUser(cmd.arguments[0])
+	if err != nil {
+		log.Fatalf("Couldn't login user: %v", err)
+	}
+	fmt.Printf("Logged in user: %v", cmd.arguments)
+	return nil
+}
+
+type commands struct {
+	c map[string]func(*state, command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	return nil
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	// Placeholder comment
+}
 
 func main() {
 	cfg, err := config.Read()
